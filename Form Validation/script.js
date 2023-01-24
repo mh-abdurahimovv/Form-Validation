cfc1ee5b27fb4ac50fdd35058049caa594df6427
Huseyn <abdurahimovm047@gmail.com>
2023-01-24 14:18:51 +0300

    Optimized code

--- Form Validation/script.js
+++ Form Validation/script.js
@@ -1,60 +1,73 @@
 
-const form = document.getElementById('form'); 
-const username = document.getElementById('username'); 
-const email = document.getElementById('email'); 
-const password = document.getElementById('password'); 
-const passwordTwo = document.getElementById('password2'); 
-const small = document.getElementById('small')
-const small2 = document.getElementById('small2')
-let small3 = document.getElementById('small3')
-const small4 = document.getElementById('small4')
+const form = document.getElementById('form'),
+username = document.getElementById('username'),
+email = document.getElementById('email'),
+password = document.getElementById('password'),
+passwordTwo = document.getElementById('password2'),
+userNameMessage = document.getElementById('small'),
+small2 = document.getElementById('small2'),
+passwordMessage = document.getElementById('small3'),
+secondPasswordMessage = document.getElementById('small4')
 
 
 
 form.addEventListener('submit', (e) => {
 
+    e.preventDefault(e);
+
+
+    // UserName Validation 
+    let message = 'Good username:)',
+        color  = 'green';
+
     if (username.value === '' || username.value === null) {
-        small.innerHTML = 'Username is required!';
-        small.style.color = 'red'
+        message = 'Username is required!'
+        color = 'red'
         
     } else if 
         (username.value.length <= 5) {
-        small.innerHTML = 'It is not a valid Username!'
-        small.style.color = 'red'
+        message = 'It is not a valid Username!'
+        color = red;
 
-    } else {
-        small.innerHTML = 'Good username:)'
-        small.style.color = 'green'
-    }
+    } 
+    userNameMessage.innerHTML = message,
+    userNameMessage.style.color = color;
 
+
+
+    //Password validation
+     message = 'Psaawords match!',
+     color = 'green';
 
 
     if (password.value.length <= 5 || null ) {
-        small3.innerHTML = 'Password must be longer than 6 charachters!'
-        small3.style.color = 'red' 
+        message = 'Password must be longer than 6 charachters!'
+        color = 'red' 
 
     } 
 
      else if  (password.value.length > 5) {
-        small3.innerHTML = 'Password is strong enough:)';
-        small3.style.color = 'Green'
+        message = 'Password is strong enough:)';
+        color = 'Green'
 
-    } else {
-        small3.innerHTML = 'Passwords match!'
-        small3.style.color = 'green';
-
-    }
+    } 
+    passwordMessage.innerHTML = message,
+    passwordMessage.style.color = color;
     
 
 
 
+     // SecondPassword validation
+     message = '',
+     color = ''; 
+
     if (passwordTwo.value === password.value) {
-        small4.innerHTML = 'Passwords match!'
-        small4.style.color = 'green';
+        message = 'Passwords match!'
+        color = 'green';
 
     } else if (passwordTwo.value != password.value) {
-        small4.innerHTML = 'Passwords do not match(:'
-        small4.style.color = 'red';
+        message = 'Passwords do not match(:'
+        color = 'red';
 
     } else {
         small4.innerHTML = ''
